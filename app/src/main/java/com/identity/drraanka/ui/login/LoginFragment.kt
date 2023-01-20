fba48255c971678e7c2099a33b5c96d3b187bbe9
Mohammed Javid <mohammed.javid@pinkerton.com>
2023-01-20 21:58:55 +0530

    Added Login UI and it Components

--- app/src/main/java/com/identity/drraanka/ui/login/LoginFragment.kt
+++ app/src/main/java/com/identity/drraanka/ui/login/LoginFragment.kt
@@ -1,21 +1,23 @@
-package com.identity.drraanka
+package com.identity.drraanka.ui.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.identity.drraanka.databinding.FragmentLoginBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
